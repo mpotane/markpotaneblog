7edc707699d0332da4e8dc10cf36578dfc7532ae
mpotane <1000100ten@gmail.com>
2025-05-22 20:39:27 +0800

    Refactor Astro configuration to integrate Tailwind CSS and add global styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [tailwind()]
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
