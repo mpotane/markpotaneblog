02636a50436ef286e4b19be0bc16a28f0018effb
mpotane <1000100ten@gmail.com>
2026-05-10 15:00:10 +0800

    Make blogpost typesafe

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,16 +2,20 @@ import { defineCollection } from "astro:content";
 import { z } from "zod";
 import { glob } from "astro/loaders";
 
+export const BlogPostSchema = z.object({
+    title: z.string(),
+    seoTitle: z.string(),
+    seoDescription: z.string(),
+    slug: z.string(),
+    datePublished: z.coerce.date(),
+    tags: z.array(z.string()),
+});
+
+export type BlogPostData = z.infer<typeof BlogPostSchema>;
+
 const blog = defineCollection({
     loader: glob({ pattern: "*.md", base: "./src/blog" }),
-    schema: z.object({
-        title: z.string(),
-        seoTitle: z.string(),
-        seoDescription: z.string(),
-        slug: z.string(),
-        datePublished: z.coerce.date(),
-        tags: z.array(z.string()),
-    }),
+    schema: BlogPostSchema,
 });
 
 export const collections = { blog };
